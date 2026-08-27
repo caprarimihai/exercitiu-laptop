d7b3d33426e0896bf71abeda71b5adac4761a520
caprarimihai <caprarimihail@gmail.com>
2026-08-28 01:35:36 +0300

    Add value at Laptop properties and write sout for him

--- src/main/java/primul_exercitiu/ExercituiLaptop/rest/LaptopController.java
+++ src/main/java/primul_exercitiu/ExercituiLaptop/rest/LaptopController.java
@@ -1,5 +1,7 @@
 package primul_exercitiu.ExercituiLaptop.rest;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -7,4 +9,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class LaptopController {
 
 
+    @Value("${laptop.brand}")
+    private String brand;
+
+    @Value("${laptop.model}")
+    private String model;
+
+    @Value("${laptop.ram}")
+    private String ram;
+
+    @Value("${laptop.processor}")
+    private String processor;
+
+
+    @GetMapping("/laptop")
+    public String getLaptop() {
+        return "Brand: " + brand +
+                "Model: " + model +
+                "RAM: " + ram +
+                "Processor: " + processor;
+
+
+    }
 }
